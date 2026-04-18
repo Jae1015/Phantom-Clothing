50066143477a2bd926f575d34166b46e4bed579f
Jae1015 <jaefly786@gmail.com>
2026-04-18 13:03:13 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,14 +25,14 @@ export const botConfig = {
     // 1 = Streaming
     // 2 = Listening
     // 3 = Watching
-    // 4 = Custom
+    // 4 = Creating Ourfits
     // 5 = Competing
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Making Clothes Rn",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
